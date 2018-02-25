62aae69d89adfb01f149593a018796bf939c21bc
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-02-25 21:02:56 +0000

    First build

---
+++ README.md
@@ -0,0 +1,12 @@
+# Studio Ghibli API
+
+> [Tania Rascia's How to Connect to an API with JavaScript article](https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/)
+
+![Logo of the project](./images/ghibliapp.png)
+
+A simple web app that connects to a Studio Ghibli API, retrieves the data with JavaScript, and displays it on the front end of a website.
+
+Updated to use fetch API, widely [supported](https://caniuse.com/#feat=fetch) in most modern browsers.
+Note a [polyfill](https://github.com/github/fetch) is available, but as this is only a demo I haven't included it.
+
+logo.png included from [Tania Rascia's ghibli project](https://github.com/taniarascia/sandbox/tree/master/ghibli)
---
+++ scripts.js
@@ -0,0 +1,54 @@
+const URL = 'https://ghibliapi.herokuapp.com/films';
+const app = document.querySelector('#root');
+
+// Add logo and container elements to the page
+const logo = document.createElement('img');
+logo.src = './images/logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+const buildCard = ({title, description}) => {
+    // create a div with card class
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card');
+
+    // Create an h1 and set text content to film titel
+    const h1 = document.createElement('h1');
+    h1.textContent = title;
+
+    card.appendChild(h1);
+
+    // Create a p and set the text content to the film's description
+    const p = document.createElement('p');
+    p.textContent = `${description.substring(0, 300)}..`;
+
+    card.appendChild(p);
+
+    // Append the card to the container element
+    container.appendChild(card);
+
+};
+
+// Get studio data using Fetch api
+fetch(URL)
+    .then(res => {
+        if (res.ok) {
+            return res.json()
+        }
+        return Promise.reject(new Error (
+            `Network response was not ok - Status ${res.status}-${res.statusText}`
+        )
+        );
+    })
+    .then(data => data.forEach( buildCard ))
+    .catch(error => {
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `It broke! ${error}`;
+        app.appendChild(errorMessage);
+    }
+    );
+
